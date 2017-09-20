d10c9ea2e1b354022f2ddfb19c6cacfc207d2190
Mark Craddock <32067310+mcraddock-ons@users.noreply.github.com>
2017-09-20 12:19:17 +0100

    Tidy up

--- ONSmod/bugherd.js
+++ ONSmod/bugherd.js
@@ -1,6 +1,6 @@
 (function (d, t) {
   var bh = d.createElement(t), s = d.getElementsByTagName(t)[0];
   bh.type = 'text/javascript';
-  bh.src = 'https://www.bugherd.com/sidebarv2.js?apikey=aidxv0q8eopldmutg6jdww';
+  bh.src = 'https://www.bugherd.com/sidebarv2.js?apikey=KEYKEYKEYEKYKEY';
   s.parentNode.insertBefore(bh, s);
   })(document, 'script');
